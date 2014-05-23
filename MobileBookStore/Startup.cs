88b623e11cf8978119c4e6a50db3cf7432658cba
Karolis Stasaitis <stkarolis@gmail.com>
2014-05-23 19:29:24 +0300

    Initial commit

---
+++ MobileBookStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobileBookStore.Startup))]
+namespace MobileBookStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
